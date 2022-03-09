3a93116141fcbc35572aad2b11c3d0e3fd36124f
liamoctavio <ocmolina47@gmail.com>
2022-03-09 19:18:02 -0300

    Proyecto2
    aa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HolaMundoComponent } from './hola-mundo/hola-mundo.component';
+import { Prueba1Component } from './prueba1/prueba1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HolaMundoComponent,
+    Prueba1Component
   ],
   imports: [
     BrowserModule
